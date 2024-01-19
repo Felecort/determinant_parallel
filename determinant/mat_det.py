f2777fce4e55af57c93220461572941bbf874ebf
vadim <dotpointen@gmail.com>
2024-01-19 16:32:07 +0300

    added an estimate of the execution time

--- determinant/mat_det.py
+++ determinant/mat_det.py
@@ -1,12 +1,16 @@
 import numpy as np
 import pandas as pd
+from time import time
 
 if __name__ == "__main__":
     df = pd.read_csv("generated_matrix.txt", sep=",", header=None)
     df.drop(columns=df.shape[0] , inplace=True)
 
     arr = np.array(df.values)
-
+    start = time()
     det = np.linalg.det(arr)
+    stop = time()
 
+    print("\n\nshape: ", arr.shape)
     print(f"DETERMINANT:", det)
+    print("Time:", stop - start)
