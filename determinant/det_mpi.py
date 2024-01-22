01f5a19d6bcff02d2f28a55cf86733439e4fb971
vadim <dotpointen@gmail.com>
2024-01-22 08:05:44 +0300

    pep8 refactor

--- determinant/det_mpi.py
+++ determinant/det_mpi.py
@@ -43,87 +43,92 @@ def triangalization(main_arr, buffer, shape, main_arr_index, lines):
             buffer[row * shape + col] = coef * main_arr[col] + buffer[row * shape + col]
 
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
+if __name__ == "__main__":
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
 
-shape = None
-
-if rank == 0:
-    shape = int(input('Enter a matrix shape: '))
-    print("Allocation...")
-    matrix = np.zeros(shape * shape, dtype=np.double)
-    fill_matrix(matrix, shape)
-    print("Matrix created!")
-    if PRINT_MATRIX:
-        print_matrix(matrix, shape)
-    print("\n")
-    start_time = MPI.Wtime()
-
-shape = comm.bcast(shape, root=0)
-for main_arr_index in range(shape - 1):
-    div_ = (shape - main_arr_index - 1) // size
-    mod_ = (shape - main_arr_index - 1) % size
-    main_arr = np.zeros(shape, dtype=np.double)
-    if rank < mod_:
-        lines = div_ + 1
-    else:
-        lines = div_
-    buffer = np.zeros(shape * lines, dtype=np.double)
+    shape = None
 
     if rank == 0:
-        main_arr = matrix[main_arr_index * shape:main_arr_index * shape + shape]
-        for rank_id in range(1, size):
-            if rank_id < mod_:
-                inner_lines = div_ + 1
-            else:
-                inner_lines = div_
-            if (rank_id < mod_) and (inner_lines != 0):
-                slice_ = slice(shape * (main_arr_index + 1 + rank_id * inner_lines), shape * (main_arr_index + 1 + rank_id * inner_lines) + shape *inner_lines)
-                comm.Send([matrix[slice_], MPI.DOUBLE], rank_id)
-            elif inner_lines != 0:
-                slice_ = slice(shape * (main_arr_index + 1 + rank_id * inner_lines + mod_), shape * (main_arr_index + 1 + rank_id * inner_lines + mod_) + shape * inner_lines)
-                comm.Send([matrix[slice_], MPI.DOUBLE], rank_id)
-        slice_ = slice(shape * (main_arr_index + 1), shape * (main_arr_index + 1) + shape * lines)
-        buffer = matrix[slice_]
-    if (lines != 0) and (rank != 0):
-        comm.Recv([buffer, MPI.DOUBLE], 0, 0)
-    comm.Bcast(main_arr, 0)
+        shape = int(input('Enter a matrix shape: '))
+        print("Allocation...")
+        matrix = np.zeros(shape * shape, dtype=np.double)
+        fill_matrix(matrix, shape)
+        print("Matrix created!")
+        if PRINT_MATRIX:
+            print_matrix(matrix, shape)
+        print("\n")
+        start_time = MPI.Wtime()
 
-    triangalization(main_arr, buffer, shape, main_arr_index, lines)
-    
-    if (lines != 0) and (rank != 0):
-        comm.Send([buffer, MPI.DOUBLE], 0, 0)
-    
-    if rank == 0:
-        slice_ = slice((main_arr_index + 1) * shape, (main_arr_index + 1) * shape + shape * lines)
-        matrix[slice_] = buffer
-        
-        for rank_id in range(1, size):
-            if rank_id < mod_:
-                inner_lines = div_ + 1
-            else:
-                inner_lines = div_
+    shape = comm.bcast(shape, root=0)
+    for main_arr_index in range(shape - 1):
+        div_ = (shape - main_arr_index - 1) // size
+        mod_ = (shape - main_arr_index - 1) % size
+        main_arr = np.zeros(shape, dtype=np.double)
+        if rank < mod_:
+            lines = div_ + 1
+        else:
+            lines = div_
+        buffer = np.zeros(shape * lines, dtype=np.double)
 
-            recieve_buffer = np.zeros(shape * inner_lines, np.double)
+        if rank == 0:
+            main_arr = matrix[main_arr_index * shape:main_arr_index * shape + shape]
+            for rank_id in range(1, size):
+                
+                if rank_id < mod_:
+                    inner_lines = div_ + 1
+                else:
+                    inner_lines = div_
+                
+                if (rank_id < mod_) and (inner_lines != 0):
+                    slice_ = slice(shape * (main_arr_index + 1 + rank_id * inner_lines), shape * (main_arr_index + 1 + rank_id * inner_lines) + shape *inner_lines)
+                    comm.Send([matrix[slice_], MPI.DOUBLE], rank_id)
+                elif inner_lines != 0:
+                    slice_ = slice(shape * (main_arr_index + 1 + rank_id * inner_lines + mod_), shape * (main_arr_index + 1 + rank_id * inner_lines + mod_) + shape * inner_lines)
+                    comm.Send([matrix[slice_], MPI.DOUBLE], rank_id)
             
-            if (rank_id < mod_) and (inner_lines != 0):
-                comm.Recv([recieve_buffer, MPI.DOUBLE], rank_id, 0)
-                slice_ = slice(shape * (main_arr_index + 1 + rank_id * inner_lines), shape * (main_arr_index + 1 + rank_id * inner_lines) + shape * inner_lines)
-                matrix[slice_] = recieve_buffer
-            elif (inner_lines != 0):
-                slice_ = slice(shape * (main_arr_index + 1 + rank_id * inner_lines + mod_), shape * (main_arr_index + 1 + rank_id * inner_lines + mod_) + shape * inner_lines)
-                comm.Recv([recieve_buffer, MPI.DOUBLE], rank_id, 0)
-                matrix[slice_] = recieve_buffer
+            slice_ = slice(shape * (main_arr_index + 1), shape * (main_arr_index + 1) + shape * lines)
+            buffer = matrix[slice_]
+        
+        if (lines != 0) and (rank != 0):
+            comm.Recv([buffer, MPI.DOUBLE], 0, 0)
+        comm.Bcast(main_arr, 0)
 
-if rank == 0:
-    res = matrix[0]
-    for mid in range(1, shape):
-        res *= matrix[shape * mid + mid]
-    
-    stop_time = MPI.Wtime()
-    
-    print("DETERMINANT: ", res)
-    print(f"Time: {stop_time - start_time:.2f}")
-    with open("result.txt", "w") as f:
-        f.write(str(res))
+        triangalization(main_arr, buffer, shape, main_arr_index, lines)
+        
+        if (lines != 0) and (rank != 0):
+            comm.Send([buffer, MPI.DOUBLE], 0, 0)
+        
+        if rank == 0:
+            slice_ = slice((main_arr_index + 1) * shape, (main_arr_index + 1) * shape + shape * lines)
+            matrix[slice_] = buffer
+            
+            for rank_id in range(1, size):
+                if rank_id < mod_:
+                    inner_lines = div_ + 1
+                else:
+                    inner_lines = div_
+
+                recieve_buffer = np.zeros(shape * inner_lines, np.double)
+                
+                if (rank_id < mod_) and (inner_lines != 0):
+                    comm.Recv([recieve_buffer, MPI.DOUBLE], rank_id, 0)
+                    slice_ = slice(shape * (main_arr_index + 1 + rank_id * inner_lines), shape * (main_arr_index + 1 + rank_id * inner_lines) + shape * inner_lines)
+                    matrix[slice_] = recieve_buffer
+                elif (inner_lines != 0):
+                    slice_ = slice(shape * (main_arr_index + 1 + rank_id * inner_lines + mod_), shape * (main_arr_index + 1 + rank_id * inner_lines + mod_) + shape * inner_lines)
+                    comm.Recv([recieve_buffer, MPI.DOUBLE], rank_id, 0)
+                    matrix[slice_] = recieve_buffer
+
+    if rank == 0:
+        res = matrix[0]
+        for mid in range(1, shape):
+            res *= matrix[shape * mid + mid]
+        
+        stop_time = MPI.Wtime()
+        
+        print("DETERMINANT: ", res)
+        print(f"Time: {stop_time - start_time:.2f}")
+        with open("result.txt", "w") as f:
+            f.write(str(res))
